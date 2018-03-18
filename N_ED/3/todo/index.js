405412d85741f527350525f33cf8c453890fd52c
kennygt51 <ken.al.15437@gmail.com>
2018-03-18 11:53:34 +0900

    WCE

--- N_ED/3/todo/index.js
+++ N_ED/3/todo/index.js
@@ -1,6 +1,26 @@
 'use strict';
 
 const tasks = new Map();
+const fs       = require('fs');
+const fileName = './tasks.json';
+
+// 同期的にファイルから復元
+try {
+    const data = fs.readFileSync(fileName, 'utf8');
+    tasks = new Map(JSON.parse(data));
+} catch (ignore) {
+    console.log(fileName + 'から復元できませんでした');
+}
+
+/**
+* タスクをファイルに保存する
+*/
+function saveTasks() {
+  // tasks という連想配列を Array.from で配列に変換
+  // JSON.stringify という関数で JSON の文字列に変換
+  // 同期的にファイルに書き出す
+  fs.writeFileSync(fileName,JSON.stringify(Array.from(tasks)),'utf8');
+}
 
 /**
 * TODOを追加する
@@ -9,6 +29,7 @@ const tasks = new Map();
 
 function todo(task) {
   tasks.set(task,false);
+  saveTasks();
 }
 
 /**
@@ -29,6 +50,19 @@ function isNotDone(taskAndIsDonePair) {
   return !isDone(taskAndIsDonePair);
 }
 
+
+/**
+* TODOを完了状態にする
+* @param {string} task
+*/
+
+function done(task) {
+  if (tasks.has(task)) {
+    tasks.set(task,true);
+    saveTasks();
+  }
+}
+
 /**
 * TODOの一覧の配列を取得する
 * @return {array}
@@ -40,8 +74,32 @@ function list() {
     .map(t => t[0]);
 }
 
+/**
+* 完了済TODOの一覧の配列を取得する
+* @return {array}
+*/
+
+function donelist() {
+  return Array.from(tasks)
+    .filter(isDone)
+    .map(t => t[0]);
+}
+
+/**
+* 項目を削除する
+* @param {string} task
+*/
+function del(task) {
+  tasks.delete(task);
+  saveTasks();
+}
+
+
 // todoという関数をパッケージの関数として外部に公開する実装
 module.exports = {
-  todo: todo
-  list: list
+  todo: todo,
+  list: list,
+  done: done,
+  donelist: donelist,
+  del: del
 };
